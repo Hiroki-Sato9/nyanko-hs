445f0df8304c88131c2308941a4d750152df2947
Hiroki Sato <resumi30minutes@gmail.com>
2024-08-17 15:35:39 +0900

    DBの接続

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/db-test', function () {
+    try {
+        $results = DB::select('SELECT DATABASE()');
+        return 'Connected to database: ' . $results[0]->{'DATABASE()'};
+    } catch (\Exception $e) {
+        return 'Connection failed: ' . $e->getMessage();
+    }
 });
